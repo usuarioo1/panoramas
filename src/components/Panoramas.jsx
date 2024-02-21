73911eaf7a86ca325ad69451e98bd24fa70e6569
unknown <nicolasgonzalezmoller@gmail.com>
2024-02-20 21:30:36 -0300

    img

--- src/components/Panoramas.jsx
+++ src/components/Panoramas.jsx
@@ -18,7 +18,7 @@ const Panoramas = () => {
                     }}>
                         <CardActionArea>
                             {/* Aumenta la altura de la imagen */}
-                            <CardMedia component="img" width={'100%'} height={300}  image={'/src/assets/img/diner.jpg'} alt='imagen' />
+                            <CardMedia component="img" width={'100%'} height={300}  image={'https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F3f23e1a0-6da5-4a06-b6e2-652d67bf731b_1600x903.png'} alt='imagen' />
                             <CardContent>
                                 <Typography variant='h3'>Panoramas</Typography>
                             </CardContent>
