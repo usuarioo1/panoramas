f36e7294c7ee7ee0050dc211ce189b1b72dc50ce
unknown <nicolasgonzalezmoller@gmail.com>
2024-03-20 21:20:02 -0300

    title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <Grid item xs={12} sm={8} md={6}>
                 <Paper sx={{ mt: 5, p: 2 }}>
                     <Typography sx={{ fontSize: 30, textAlign: 'center' }}>
-                        App dedicada para organizar los panoramas o juntitas, ya sean panoramas o K-panoramas
+                        App dedicada para organizar panoramas
                     </Typography>
                 </Paper>
             </Grid>
